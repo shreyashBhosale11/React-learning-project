0d459d04eb325d067155c95abd4e9ff2c0cad1da
shreyashBhosale11 <shreyashbhosale482005@gmail.com>
2026-01-07 12:29:54 +0530

    07 done by chai aur code

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/footer/Footer'
+import About from './components/About/About'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
